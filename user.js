0f2759198511572fc7bdb85496170718bb9a226e
kr-sharma28 <krishnapurohit417400@gmail.com>
2025-01-25 15:51:53 +0530

    file

--- user.js
+++
@@ -1,9 +0,0 @@
-let mongoose = require("mongoose");
-
-let userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
-
-module.exports = mongoose.model("User", userSchema);
